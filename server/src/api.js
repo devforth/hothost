d5c0a5cf5087047397404e57fcdcddd019e1fe2f
MultiFox200 <vprotasenia@devforth.io>
2022-05-03 16:54:55 +0300

    Fix interval in agent. Save data to prisma

--- server/src/api.js
+++ server/src/api.js
@@ -15,9 +15,22 @@ router.post('/add_monitor', mustBeAuthorizedView(async (req, res) => {
 }));
 
 router.post('/data/:secret', async (req, res) => {
-    const data = req.fields;
-    console.log(data);
-    res.send('TODO');
+    const monitorData = req.fields;
+    console.log(monitorData);
+
+    const md = await prisma.monitoringData.findUnique({ where: { id: req.params.secret } });
+    if (!md) {
+        res.statusCode = 401;
+        res.send('Unauthorized');
+    } else {
+        const data = Object.keys(monitorData).reduce((acc, key) => {
+            const value = monitorData[key];
+            acc[key] = value !== undefined && value !== null ? value.toString() : value;
+            return acc;
+        }, {});
+        await prisma.monitoringData.update({ data, where: { id: req.params.secret } });
+        res.send('OK');
+    }
 });
 
 module.exports = router;
