9bc8ea6a9878c5c13db584e0ad9d508fc3419b10
IhorStorozhok <storozhokigor@gmail.com>
2022-10-13 11:49:16 +0300

    some styling

--- server/frontend/src/main.jsx
+++ server/frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import "../public/output.css";
+import "flowbite/dist/flowbite.min.css";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+);
---
+++ server/frontend/src/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      'mobile': {'max': '640px'},
+    },
+  },
+  darkMode: 'class',
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('flowbite/plugin'),
+    require('@tailwindcss/typography'),
+  ],
+}
