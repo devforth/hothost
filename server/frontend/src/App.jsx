8324f79fdf59efd90a17522b47aeddc310d9cb98
IhorStorozhok <storozhokigor@gmail.com>
2022-10-20 15:52:52 +0300

    add routing

--- server/frontend/src/App.jsx
+++ server/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { getData } from "../FetchApi.js";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 import { createContext } from "react";
@@ -10,6 +10,9 @@ import Home from "./Components/Home/Home.jsx";
 import Login from "./Components/Login/Login";
 import Settings from "./Components/Settings/Settings";
 import Users from "./Components/Users/Users.jsx";
+import HttpMonitor from "./Components/HttpMonitor/HttpMonitor";
+import Plugins from "./Components/Plugins/Plugins";
+import Plugin from "./Components/Plugin/Plugin";
 
 import "./App.css";
 
@@ -17,38 +20,28 @@ function App() {
   //get data from server
   const [isAuthorize, setIsAuthorize] = useState(true);
   const [monitoringData, setMonitoringData] = useState([]);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getData("getMonitoringData");
-      if (!data.error) {
-        data && setMonitoringData(data);
-      } else {
-        setIsAuthorize(false);
-        navigate("/login");
-      }
-    };
-    fetchData();
-  }, []);
 
   return (
     <div className="App">
       <Header isAuthorize={isAuthorize} />
-
-      <Routes>
-        <Route
-          path="/"
-          element={isAuthorize ? <Home /> : <Navigate replace to="/login" />}
-        />
-        <Route
-          path="/home"
-          element={isAuthorize ? <Home /> : <Navigate replace to="/login" />}
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/users" element={<Users />} />
-      </Routes>
+      <Suspense fallback={<div>Download</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={isAuthorize ? <Home /> : <Navigate replace to="/login" />}
+          />
+          <Route path="/home" element={<Home />}>
+            <Route path="http-monitor" element={<HttpMonitor />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/plugins" element={<Plugins />} />
+          <Route path="/plugin" element={<Plugin />}>
+            <Route path=":pluginName" element={<Plugin />}></Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
