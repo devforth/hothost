7b164606630cac1d3ac0fc272b0bc5e34dda31c7
IhorStorozhok <storozhokigor@gmail.com>
2022-10-26 13:26:07 +0300

    add dropdoun closing by click on emty vp zone

--- server/frontend/src/Components/Header/Header.jsx
+++ server/frontend/src/Components/Header/Header.jsx
@@ -64,38 +64,40 @@ const Header = ({ theme, setTheme }) => {
               </a>
             ) : (
               <>
-                <div
-                  onClick={() => {
-                    setSettingsIsVisible(!setingsIsVisible);
-                  }}
-                >
-                  <svg
-                    className="my-1 w-6 h-6 text-gray-700 dark:text-gray-300 h-8 w-8 cursor-pointer hover:text-gray-600"
-                    fill="none"
-                    stroke="currentColor"
-                    data-dropdown-toggle="settingsDropdown"
-                    data-dropdown-placement="bottom-end"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                    ></path>
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                    ></path>
-                  </svg>
-                </div>
+                {" "}
                 <OutsideHider
                   state={setingsIsVisible}
                   setstate={setSettingsIsVisible}
                 >
+                  <div
+                    onClick={() => {
+                      setSettingsIsVisible(!setingsIsVisible);
+                    }}
+                  >
+                    <svg
+                      className=" text-gray-700 dark:text-gray-300 h-10 w-10 cursor-pointer hover:text-gray-600"
+                      fill="none"
+                      stroke="currentColor"
+                      data-dropdown-toggle="settingsDropdown"
+                      data-dropdown-placement="bottom-end"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                      ></path>
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      ></path>
+                    </svg>
+                  </div>
+
                   {setingsIsVisible ? (
                     <Settings
                       clickAndNavigate={clickAndNavigate}
@@ -103,34 +105,34 @@ const Header = ({ theme, setTheme }) => {
                     ></Settings>
                   ) : null}
                 </OutsideHider>
-
-                <div
-                  ref={logoutMenuBtn}
-                  className="px-2"
-                  onClick={() => {
-                    setUsertManageIsVisible(!usertManageIsVisible);
-                  }}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    id="userManage"
-                    className="text-gray-700 dark:text-gray-300 h-10 w-10 cursor-pointer hover:text-gray-600 "
-                    data-dropdown-toggle="userManageDropdown"
-                    data-dropdown-placement="bottom-end"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </div>
                 <OutsideHider
                   state={usertManageIsVisible}
                   setstate={setUsertManageIsVisible}
                 >
+                  <div
+                    ref={logoutMenuBtn}
+                    className="px-2"
+                    onClick={() => {
+                      setUsertManageIsVisible(!usertManageIsVisible);
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      id="userManage"
+                      className="text-gray-700 dark:text-gray-300 h-10 w-10 cursor-pointer hover:text-gray-600 "
+                      data-dropdown-toggle="userManageDropdown"
+                      data-dropdown-placement="bottom-end"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </div>
+
                   {usertManageIsVisible ? (
                     <div
                       id="userManageDropdown"
@@ -163,58 +165,9 @@ const Header = ({ theme, setTheme }) => {
                 </OutsideHider>
               </>
             )}
-
-            {/* <button data-collapse-toggle="mobile-menu-4" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-4" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-          <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-        </button>  */}
           </div>
         </div>
       </nav>
-
-      {/* <script>
-    var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-    var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-
-    // Change the icons inside the button based on previous settings
-    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        themeToggleLightIcon.classNameList.remove('hidden');
-    } else {
-        themeToggleDarkIcon.classNameList.remove('hidden');
-    }
-
-    var themeToggleBtn = document.getElementById('theme-toggle');
-
-    themeToggleBtn.addEventListener('click', function() {
-
-    // toggle icons inside button
-    themeToggleDarkIcon.classNameList.toggle('hidden');
-    themeToggleLightIcon.classNameList.toggle('hidden');
-
-    // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classNameList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        } else {
-            document.documentElement.classNameList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        }
-
-    // if NOT set via local storage previously
-    } else {
-        if (document.documentElement.classNameList.contains('dark')) {
-            document.documentElement.classNameList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        } else {
-            document.documentElement.classNameList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        }
-    }
-    
-    });
-  </script> */}
     </div>
   );
 };
