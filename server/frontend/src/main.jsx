63051d8127cd7b9ffbc6137da308e7c806f10916
IhorStorozhok <storozhokigor@gmail.com>
2022-11-04 11:28:09 +0200

    fixed chart style

--- server/frontend/src/main.jsx
+++ server/frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-// import "../public/output.css";
+import "./output.css"
 import "flowbite/dist/flowbite.min.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
