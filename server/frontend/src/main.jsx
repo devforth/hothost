84fb2bd36a70d0807b3f3c205fef84c690032d9f
IhorStorozhok <storozhokigor@gmail.com>
2022-11-07 13:26:49 +0200

    add input styling to index css

--- server/frontend/src/main.jsx
+++ server/frontend/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import "./output.css"
+
+
 import "flowbite/dist/flowbite.min.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
