0dfcfee463dab89e3715f5a93d0d3e9f5465e701
IhorStorozhok <storozhokigor@gmail.com>
2022-12-27 16:21:41 +0200

    deletr consols

--- server/src/plugins/telegram.js
+++ server/src/plugins/telegram.js
@@ -148,7 +148,6 @@ Chat in which you send message will be used to publish notifications.
         headers: { "Content-Type": "application/json" },
       }
     ).then((r) => r.json());
-    console.log("TG secode resp", secondResp);
   },
 
   async onPluginEnabled() {
