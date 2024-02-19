b78e5ee81f375bfa929a928556b7ace2a69f7db4
allmors <jesmora@protonmail.com>
2024-02-19 10:23:26 +0800

    Analyze top RAM hidden from guest

--- server/frontend/src/Components/MonitoringTable/MonitoringRow.jsx
+++ server/frontend/src/Components/MonitoringTable/MonitoringRow.jsx
@@ -94,29 +94,31 @@ const MonitoringRow = (props) => {
               RAM:{" "}
               {`${host.ram_total} 
                   ${host.ram_used_percentage}% used`}
-              <button
-                id={host.id}
-                data-modal-toggle="modal_timeline-{{@index}}"
-                className="ml-2"
-                onClick={(e) => {
-                  setChosenHost(e.currentTarget.id);
-                  setDonutModalIsVisible(true);
-                  setDotsIsvisible(false);
-                }}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="w-5 h-5"
+              {cookieExist ? (
+                <button
+                  id={host.id}
+                  data-modal-toggle="modal_timeline-{{@index}}"
+                  className="ml-2"
+                  onClick={(e) => {
+                    setChosenHost(e.currentTarget.id);
+                    setDonutModalIsVisible(true);
+                    setDotsIsvisible(false);
+                  }}
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </button>
+              ) : null}
             </span>
           </Tooltip>
           {host.ram_warning && (
